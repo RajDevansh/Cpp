827d54953118618fc0debaacb989d6402a9e4851
RajDevansh <devanshrajpoot2008@gmail.com>
2025-08-09 18:26:43 +0530

    Add files via upload

---
+++ TypeCasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float a=23.34;
+    int b=34;
+    cout<<"Converting float to int: "<<(int)a<<endl; 
+    cout<<"Converting float to int: "<<int(a)<<endl;
+    cout<<"Converting int to float: "<<(float)b<<endl;
+    cout<<"Converting int to float: "<<float(b)<<endl;
+    return 0;
+}
