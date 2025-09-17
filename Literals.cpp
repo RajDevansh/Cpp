39094216f25390f6e4c29077c7a23197ba484447
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-17 22:25:33 +0530

    Add files via upload

---
+++ Literals.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+float d=34.4f;
+long double e=34.4l;
+int main(){
+    cout<<"Size of 34.4:"<<sizeof(34.4)<<endl;
+    cout<<"Size of 34.4f:"<<sizeof(34.4f)<<endl;
+    cout<<"Size of 34.4l:"<<sizeof(34.4l)<<endl;
+    return 0;
+}
