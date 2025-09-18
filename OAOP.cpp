b32d5b0931f8636a6261dd6c161fcce1c37dd04d
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-18 22:24:20 +0530

    Add files via upload

---
+++ OAOP.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int a=4;
+int b=5;
+int c=6;
+int main(){
+    int d=a*b+c;
+    int e=((((a*5)+b)-c)+d);
+    cout<<"The value of d is: "<<d<<endl;
+    cout<<"The value of e is: "<<e<<endl;
+    return 0;
+}
