6575b389929407e8af675a8a4134bef7d8a5e993
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-19 14:47:17 +0530

    Add files via upload

---
+++ SwitchCase.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    switch(age){
+        case 3:
+        cout<<"You are a toddler."<<endl;
+        break;
+    return 0;
+}
+}
